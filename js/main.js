92caf67ba159386169f80f5d77450fb82d453295
luciaoSainz <danielsainz28@gmail.com>
2020-07-07 22:31:10 +0200

    added animation in hamburguer

--- js/main.js
+++ js/main.js
@@ -12,3 +12,9 @@ window.addEventListener("scroll", function(){
 
 //----MENU
 
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function(){
+    enlacesHeader.classList.toggle("menudos")
+})
